58597752de58a5e65d385d3df87e673c6c2906d3
kevinokello <okellokevin77@gmail.com>
2022-10-13 15:38:48 +0300

    flexible button

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,6 +8,10 @@ function Banner() {
               layout='fill'
               objectFit="cover"
         />
+        <div className="absolute top-1/2 w-full text-center">
+            <p className="text-sm sm:text-lg">cool banner</p>
+              <button className="text-red-500 bg-white px-10 py-4 shadow-md rounded-full font-bold hover:shadow-xl active:scale-90 transition duration-150 " >Flexible</button>
+        </div>
     </div>
   )
 }
