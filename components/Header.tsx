ccf8c94f50740633b061723a532a2c43adc163b1
kevinokello <okellokevin77@gmail.com>
2022-10-13 14:36:07 +0300

    header styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
-            <h1>header</h1>
-            <div>
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5">
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image 
                 src='https://links.papareact.com/qd3'
                 layout='fill'
+                objectFit='contain' objectPosition='left'
                 />
             </div>
 
@@ -19,7 +19,7 @@ function Header() {
             <div>
 
             </div>
-        </div>
+        </header>
 
     )
 }
